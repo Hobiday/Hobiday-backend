6ece02cb77361516dbafb3507aae60498e77612e
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2024-11-15 16:05:07 +0900

    Add files via upload

---
+++ hobiday-backend/build/resources/main/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, nickname, password) VALUES ('abcde1@gmail.com', '김김김', '11')
+INSERT INTO users (email, nickname, password) VALUES ('abcde2@gmail.com', '이이이', '22')
+INSERT INTO users (email, nickname, password) VALUES ('abcde3@gmail.com', '박박박', '33')
