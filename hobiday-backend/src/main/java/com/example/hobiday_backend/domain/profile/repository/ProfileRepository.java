b849951e13fe2739352443a8ea17039f8137f83d
chyun5197 <103193121+chyun5197@users.noreply.github.com>
2024-11-21 19:46:20 +0900

    Delete hobiday-backend directory

--- hobiday-backend/src/main/java/com/example/hobiday_backend/domain/profile/repository/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hobiday_backend.domain.profile.repository;
-
-import com.example.hobiday_backend.domain.profile.domain.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-}
